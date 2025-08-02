61d953d0aa081612ecf3b1f845cfbdbbbaefd990
Ivan Afanasiev <Ram1zen@yandex.ru>
2025-08-02 12:52:24 +0700

    use orjson

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from api import router as api_router
 from core.config import settings
@@ -10,16 +11,20 @@ from core.models import db_helper
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    #startapp
+    # startapp
+    # async with db_helper.engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.drop_all)
     yield
-    #shutdown
+    # shutdown
     await db_helper.dispose()
 
 
-main_app = FastAPI(lifespan=lifespan)
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
 main_app.include_router(
     api_router,
-    prefix=settings.api.prefix,
 )
 
 if __name__ == "__main__":
